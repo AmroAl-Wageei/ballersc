7bca03c2bcea8253d33d28fae53640ad9ca253e7
amro alwageei  -gt <gt.amro.alwageei@Jt.JtGroup>
2024-01-11 20:55:12 +0300

    C3

---
+++ src/components/Teams/team.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Coach from './coach'
+import Technical from './technical'
+import Nutrotion from './nutrotion'
+// import Footer from '../Footer/Footer'
+export default function team() {
+  return (
+    <div>
+    
+    <Coach />
+    <Technical />
+    <Nutrotion />
+    </div>
+  )
+}
--- src/pages/team.jsx
+++ src/pages/team.jsx
@@ -1,14 +1,17 @@
 import React from 'react'
 // import Navbar from '../components/Navbar/Navbar'
-// import Team from '../components/Teams/team'
 // import Form from '../components/Form/Form'
-// import Footer from '../components/Footer/Footer'
-// import FixedButton from '../components/FixedButton/FixedButton'
-// import ScrollToTop from '../components/ScrollToTop/top'
+import Team from '../components/Teams/team'
+import Footer from '../components/Footer/Footer'
+import FixedButton from '../components/FixedButton/FixedButton'
+import ScrollToTop from '../components/ScrollToTop/top'
 export default function team() {
   return (
     <div>
-
+      <Team />
+      <Footer />
+      <FixedButton />
+      <ScrollToTop />
     </div>
   )
 }
