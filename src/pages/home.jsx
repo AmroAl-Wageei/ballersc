5e380715f8b50a3f34e2a0db7c3591fa7f91992d
amro alwageei  -gt <gt.amro.alwageei@Jt.JtGroup>
2024-01-11 16:12:15 +0300

    C1

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -6,10 +6,11 @@ import AboutBallers from '../components/Aballers/aballers';
 import Services from '../components/services/services'
 import AfterServices from '../components/AfterServices/AfterServices'
 import Scan from '../components/Scan/scan';
-// import Map from '../components/Map/map'
+import Ballers from '../components/Ballers/ballers'
+import Gallery from '../components/Gallery/Gallery';
+import Map from '../components/Map/map'
 // import FAQ from '../components/FAQ/faq';
 // import Slider from '../components/Slider/slider'
-// import Ballers from '../components/Ballers/ballers'
 // import Footer from '../components/Footer/Footer'
 import FixedButton from '../components/FixedButton/FixedButton'
 import ScrollToTop from '../components/ScrollToTop/top'
@@ -26,6 +27,9 @@ export default function home() {
       <Services />
       <AfterServices />
       <Scan />
+      <Ballers />
+      <Gallery />
+      <Map />
       <FixedButton />
       <ScrollToTop />
     </div>
